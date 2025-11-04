5447e42a788155908acb9346286438f890930af0
tiendung62 <dungptph46011@fpt.edu.vn>
2025-11-04 19:24:56 +0700

    thêm servicer mới

---
+++ NT.WEB/Services/CategoryWebService.cs
@@ -0,0 +1,30 @@
+csharp NT.WEB\Services\CategoryWebService.cs
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using NT.BLL.Interfaces;
+using NT.BLL.Services;
+using NT.SHARED.Models;
+
+namespace NT.WEB.Services
+{
+    // Inherit BLL implementation and add web-specific helpers
+    public class CategoryWebService : CategoryService
+    {
+        public CategoryWebService(IGenericRepository<Category> repository)
+            : base(repository)
+        {
+        }
+
+        // Example helper used by Razor Pages: search by partial name
+        public Task<IEnumerable<Category>> SearchByNameAsync(string partialName)
+        {
+            if (string.IsNullOrWhiteSpace(partialName))
+                return _repository.GetAllAsync();
+
+            Expression<Func<Category, bool>> predicate = c => c.Name.Contains(partialName);
+            return _repository.FindAsync(predicate);
+        }
+    }
+}
