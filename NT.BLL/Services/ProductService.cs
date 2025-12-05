a87289d2e91f0e8a04a7b3cb199151b51e88275e
ghuy2509 <toylovecc3@gmail.com>
2025-12-05 12:56:30 +0700

    view tim kiếm

--- NT.BLL/Services/ProductService.cs
+++ NT.BLL/Services/ProductService.cs
@@ -1,15 +1,26 @@
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using NT.BLL.Interfaces;
 using NT.SHARED.Models;
 
 namespace NT.BLL.Services
 {
-    public class ProductService : GenericService<Product>, IProductService
+    public class ProductService : GenericService<Product>, IProductService, ISearchByNameService<Product>
     {
         public ProductService(IGenericRepository<Product> repository) : base(repository)
         {
+
+        }       
+        public Task<IEnumerable<Product>> SearchByNameAsync(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return _repository.GetAllAsync();
+
+            Expression<Func<Product, bool>> predicate = p => p.Name.Contains(name);
+            return _repository.FindAsync(predicate);
         }
     }
 }
+    
